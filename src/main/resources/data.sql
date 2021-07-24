519eb68235ee86c26988604b568f0d4f5a99aae9
Norivan Oliveira <norivan.oliveira@gmail.com>
2021-07-24 11:05:38 -0300

    Alteração de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Primeiro membro', 'membro@email.com', '123456');
 
-INSERT INTO CURSO(nome, categoria) VALUES('Geral', 'Regras');
-INSERT INTO CURSO(nome, categoria) VALUES('Notícias', 'Brasil');
+INSERT INTO CURSO(nome, categoria) VALUES('Freios', 'Carros');
+INSERT INTO CURSO(nome, categoria) VALUES('Eletrica', 'Carros');
 
 INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Teste 1', 'Teste 1', '2021-05-05 18:00:00', 1, 1, 1);
 INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Teste 2', 'Teste 2', '2021-05-05 19:00:00', 1, 1, 1);
